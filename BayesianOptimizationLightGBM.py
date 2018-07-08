d4ec18980a63393b55f79b1ce08a8b062dd9d82a
MitsuruFujiwara <fujiwara52jp@gmail.com>
2018-07-08 21:54:08 +0900

    update bayesian optimization

--- BayesianOptimizationLightGBM.py
+++ BayesianOptimizationLightGBM.py
@@ -1,20 +1,38 @@
 import pandas as pd
+import lightgbm
+
+from bayes_opt import BayesianOptimization
+from LightGBM_with_Simple_Features import bureau_and_balance, previous_applications, pos_cash, installments_payments, credit_card_balance, application_train_test
 
 # 以下参考
 # https://github.com/fmfn/BayesianOptimization
 # https://www.kaggle.com/tilii7/olivier-lightgbm-parameters-by-bayesian-opt/code
 
-train_target = application_train['TARGET'].values
-train_data = application_train.drop(['TARGET'], axis = 1).values
+NUM_ROWS=10000
 
-lgbm_train = lightgbm.Dataset(train_data, train_target)
-fold = StratifiedKFold(n_splits=5, shuffle=True, random_state=666)
-cv_fold = fold.split(train_data, train_target)
-params = dict()
-params["learning_rate"] = 0.02
-params["silent"] = True
-params["nthread"] = 16
-params["application"] = "binary"
+# ベストスコアのカーネルと同じ特徴量を使います
+BUREAU = bureau_and_balance(NUM_ROWS)
+PREV = previous_applications(NUM_ROWS)
+POS = pos_cash(NUM_ROWS)
+INS = installments_payments(NUM_ROWS)
+CC = credit_card_balance(NUM_ROWS)
+
+# concat datasets
+DF = application_train_test(NUM_ROWS)
+DF = DF.join(BUREAU, how='left', on='SK_ID_CURR')
+DF = DF.join(PREV, how='left', on='SK_ID_CURR')
+DF = DF.join(POS, how='left', on='SK_ID_CURR')
+DF = DF.join(INS, how='left', on='SK_ID_CURR')
+DF = DF.join(CC, how='left', on='SK_ID_CURR')
+
+
+# split test & train
+TRAIN_DF = DF[DF['TARGET'].notnull()]
+TEST_DF = DF[DF['TARGET'].isnull()]
+
+# set
+lgbm_train = lightgbm.Dataset(TRAIN_DF.drop('TARGET', axis=1),
+                              TRAIN_DF['TARGET'])
 
 def lgbm_eval(num_leaves,
               colsample_bytree,
@@ -25,6 +43,13 @@ def lgbm_eval(num_leaves,
               min_split_gain,
               min_child_weight,
              ):
+
+    params = dict()
+    params["learning_rate"] = 0.02
+    params["silent"] = True
+    params["nthread"] = 16
+    params["application"] = "binary"
+
     params["num_leaves"] = int(num_leaves)
     params['colsample_bytree'] = max(min(colsample_bytree, 1), 0)
     params['subsample'] = max(min(subsample, 1), 0)
@@ -34,28 +59,33 @@ def lgbm_eval(num_leaves,
     params['min_split_gain'] = min_split_gain
     params['min_child_weight'] = min_child_weight
 
-
-
     clf = lightgbm.cv(params=params,
                       train_set=lgbm_train,
                       metrics=["auc"],
-                      folds=cv_fold,
+                      folds=None,
                       num_boost_round=5,
-                      # early_stopping_rounds=100,
-                      verbose_eval=1,
+                      early_stopping_rounds=10,
+                      verbose_eval=1
                      )
 
     return clf['auc-mean'][-1]
 
+def main():
 
-clf_bo = BayesianOptimization(lgbm_eval, {'num_leaves': (30, 45),
-                                          'colsample_bytree': (0.1, 1),
-                                          'subsample': (0.1, 1),
-                                          'max_depth': (5, 15),
-                                          'reg_alpha': (0, 10),
-                                          'reg_lambda': (0, 10),
-                                          'min_split_gain': (0, 1),
-                                          'min_child_weight': (30, 45),
-                                        })
+    # clf for bayesian optimization
+    clf_bo = BayesianOptimization(lgbm_eval, {'num_leaves': (30, 45),
+                                              'colsample_bytree': (0.1, 1),
+                                              'subsample': (0.1, 1),
+                                              'max_depth': (5, 15),
+                                              'reg_alpha': (0, 10),
+                                              'reg_lambda': (0, 10),
+                                              'min_split_gain': (0, 1),
+                                              'min_child_weight': (30, 45),
+                                            })
 
-clf_bo.maximize(init_points=4, n_iter=20)
+    clf_bo.maximize(init_points=4, n_iter=20)
+
+    print(clf_bo['max']['max_val'])
+
+if __name__ == '__main__':
+    main()
--- LightGBM_with_Simple_Features.py
+++ LightGBM_with_Simple_Features.py
@@ -324,38 +324,37 @@ def display_importances(feature_importance_df_):
     plt.tight_layout()
     plt.savefig('lgbm_importances01.png')
 
-
 def main(debug = False):
     num_rows = 10000 if debug else None
     df = application_train_test(num_rows)
     with timer("Process bureau and bureau_balance"):
         bureau = bureau_and_balance(num_rows)
         print("Bureau df shape:", bureau.shape)
-        df = df.join(bureau, how='left', on='SK_ID_CURR') # 自分の環境でエラーが出るのでon='SK_ID_CURR'を消しました
+        df = df.join(bureau, how='left', on='SK_ID_CURR')
         del bureau
         gc.collect()
     with timer("Process previous_applications"):
         prev = previous_applications(num_rows)
         print("Previous applications df shape:", prev.shape)
-        df = df.join(prev, how='left', on='SK_ID_CURR') # 自分の環境でエラーが出るのでon='SK_ID_CURR'を消しました
+        df = df.join(prev, how='left', on='SK_ID_CURR')
         del prev
         gc.collect()
     with timer("Process POS-CASH balance"):
         pos = pos_cash(num_rows)
         print("Pos-cash balance df shape:", pos.shape)
-        df = df.join(pos, how='left', on='SK_ID_CURR') # 自分の環境でエラーが出るのでon='SK_ID_CURR'を消しました
+        df = df.join(pos, how='left', on='SK_ID_CURR')
         del pos
         gc.collect()
     with timer("Process installments payments"):
         ins = installments_payments(num_rows)
         print("Installments payments df shape:", ins.shape)
-        df = df.join(ins, how='left', on='SK_ID_CURR') # 自分の環境でエラーが出るのでon='SK_ID_CURR'を消しました
+        df = df.join(ins, how='left', on='SK_ID_CURR')
         del ins
         gc.collect()
     with timer("Process credit card balance"):
         cc = credit_card_balance(num_rows)
         print("Credit card balance df shape:", cc.shape)
-        df = df.join(cc, how='left', on='SK_ID_CURR') # 自分の環境でエラーが出るのでon='SK_ID_CURR'を消しました
+        df = df.join(cc, how='left', on='SK_ID_CURR')
         del cc
         gc.collect()
     with timer("Run LightGBM with kfold"):
