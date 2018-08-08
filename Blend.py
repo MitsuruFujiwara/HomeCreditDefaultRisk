8e8968ab907a0673da00b4e3e8d692d912d9fe8b
MitsuruFujiwara <fujiwara52jp@gmail.com>
2018-08-08 21:31:51 +0900

    LB#5

---
+++ Blend.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import numpy as np
+
+"""
+複数モデルのoutputをブレンドして最終的なsubmitファイルを生成するスクリプト。
+WEIGHTED AVERAGE RANK METHODとかいうのを使います。
+参考:https://www.kaggle.com/mfjwr1/magic-of-weighted-average-rank-0-80?scriptVersionId=4675122
+"""
+
+def main():
+    data = {}
+    filepaths=['submission_add_feature.csv','submission_add_feature_xgb.csv']
+    weights = [0.6, 0.4]
+
+    for path in filepaths:
+        data[path[:-4]] = pd.read_csv(path)
+
+    ranks = pd.DataFrame(columns=data.keys())
+
+    for key in data.keys():
+        ranks[key] = data[key].TARGET.rank(method='min')
+    ranks['Average'] = ranks.mean(axis=1)
+    ranks['Scaled Rank'] = (ranks['Average'] - ranks['Average'].min()) / (ranks['Average'].max() - ranks['Average'].min())
+    print(ranks.corr()[:1])
+
+    ranks['Score'] = ranks[['submission_add_feature','submission_add_feature_xgb']].mul(weights).sum(1) / ranks.shape[0]
+    submission_lb = pd.read_csv('submission.csv')
+    submission_lb['TARGET'] = ranks['Score']
+    submission_lb.to_csv("WEIGHT_AVERAGE_RANK.csv", index=None)
+
+if __name__ == '__main__':
+    main()
--- LightGBM_with_Simple_Features.py
+++ LightGBM_with_Simple_Features.py
@@ -564,12 +564,13 @@ def kfold_lightgbm(df, num_folds, stratified = False, debug= False):
     if not debug:
         # AUDスコアを上げるため提出ファイルの調整を追加→これは最終段階で使いましょう
         # 0or1に調整する水準を決定（とりあえず上位下位0.05%以下のものを調整）→下位は10%に変更
+        """
         q_high = test_df['TARGET'].quantile(0.9995)
         q_low = test_df['TARGET'].quantile(0.1)
 
         test_df['TARGET'] = test_df['TARGET'].apply(lambda x: 1 if x > q_high else x)
         test_df['TARGET'] = test_df['TARGET'].apply(lambda x: 0 if x < q_low else x)
-
+        """
         # 分離前モデルの予測値を保存
         test_df['TARGET'] = sub_preds
         test_df[['SK_ID_CURR', 'TARGET']].to_csv(submission_file_name, index= False)
