d643bf562829c9ddce0c74c29a48e845c9e33292
MitsuruFujiwara <fujiwara52jp@gmail.com>
2018-08-13 20:55:42 +0900

    LB#20

--- Blend.py
+++ Blend.py
@@ -16,7 +16,7 @@ import seaborn as sns
 def main():
     data = {}
     filepaths=['submission_add_feature_lgbm.csv','submission_add_feature_xgb.csv']
-    weights = [0.6, 0.4]
+    weights = [0.4, 0.6]
 
     for path in filepaths:
         data[path[:-4]] = pd.read_csv(path)
