6a415d868bc0f67c9439e7cae905bd856d693a09
MitsuruFujiwara <fujiwara52jp@gmail.com>
2018-08-29 01:34:06 +0900

    LB#47

--- Blend.py
+++ Blend.py
@@ -12,45 +12,78 @@ import seaborn as sns
 複数モデルのoutputをブレンドして最終的なsubmitファイルを生成するスクリプト。
 """
 
-# TODO　# 0-1に置き換える最適な閾値を得るやつ
-def getZeroOneThresholds(df_oof):
-    # 閾値の初期値
-    q_high = 1.0
-    q_low = 0.0
+# 0-1に置き換える最適な閾値を得るやつ
+def getZeroOneThresholds(act, pred):
 
-    #
-    for q in np.arange(0, 1.0001, 0.0001):
-        _q = df_oof['TARGET'].quantile(q)
-    q_high = df_oof['TARGET'].quantile(0.9995)
-    q_low = df_oof['TARGET'].quantile(0.0005)
+    # get optimal low
+    auc_bst = 0.0
+    for _q in np.arange(0, 0.10001, 0.0001):
+        _threshold = pred.quantile(_q)
+        _pred = pred.apply(lambda x: 0 if x < _threshold else x)
+        _auc = roc_auc_score(act, _pred)
+        if _auc > auc_bst:
+            auc_bst = _auc
+            q_low = _q
+        print("q: {:.4f}, auc: {:.10f}".format(_q, _auc))
+    print("best q_low: {:.4f}, best auc: {:.10f}".format(q_low, auc_bst))
 
-    test_df['TARGET'] = test_df['TARGET'].apply(lambda x: 1 if x > q_high else x)
-    test_df['TARGET'] = test_df['TARGET'].apply(lambda x: 0 if x < q_low else x)
+    # set low threshold
+    pred = pred.apply(lambda x: 0 if x < q_low else x)
+
+    # get optimal high
+    auc_bst = 0.0
+    for _q in np.arange(0.9, 1.0001, 0.0001):
+        _threshold = pred.quantile(_q)
+        _pred = pred.apply(lambda x: 1 if x > _threshold else x)
+        _auc = roc_auc_score(act, _pred)
+        if _auc > auc_bst:
+            auc_bst = _auc
+            q_high = _q
+        print("q: {:.4f}, auc: {:.10f}".format(_q, _auc))
+    print("best q_high: {:.4f}, best auc: {:.10f}".format(q_high, auc_bst))
 
     return q_high, q_low
 
 def main():
     # load submission files
     sub = pd.read_csv('sample_submission.csv')
-    sub_lgbm = pd.read_csv('submission_add_feature_lgbm_#38.csv')
+    sub_lgbm = pd.read_csv('submission_add_feature_lgbm_#46.csv')
     sub_xgb = pd.read_csv('submission_add_feature_xgb_#44.csv')
 
-    # loada out of fold data
+    # load out of fold data
     train_df = pd.read_csv('application_train.csv')
-    oof_lgbm = pd.read_csv('oof_lgbm_#43.csv')
+    oof_lgbm = pd.read_csv('oof_lgbm_#46.csv')
     oof_xgb = pd.read_csv('oof_xgb_#44.csv')
 
+    # change index & columns
     oof_lgbm.columns=['SK_ID_CURR', 'lgbm']
     oof_xgb.columns=['SK_ID_CURR', 'xgb']
     oof_lgbm.index=oof_lgbm['SK_ID_CURR']
     oof_xgb.index=oof_xgb['SK_ID_CURR']
     train_df.index=train_df['SK_ID_CURR']
 
+    # merge dfs
     train_df = train_df.merge(oof_lgbm, how='left', on='SK_ID_CURR')
     train_df = train_df.merge(oof_xgb, how='left', on='SK_ID_CURR')
     train_df = train_df[['SK_ID_CURR','TARGET', 'lgbm', 'xgb']].dropna()
 
-    """
+    # get thresholds
+    q_high_lgbm, q_low_lgbm = getZeroOneThresholds(train_df['TARGET'], train_df['lgbm'])
+    q_high_xgb, q_low_xgb = getZeroOneThresholds(train_df['TARGET'], train_df['xgb'])
+
+    # replace values to 0 or 1 by threshold
+    train_df['lgbm'] = train_df['lgbm'].apply(lambda x: 0 if x < q_low_lgbm else x)
+    train_df['lgbm'] = train_df['lgbm'].apply(lambda x: 1 if x > q_high_lgbm else x)
+
+    sub_lgbm['TARGET'] = sub_lgbm['TARGET'].apply(lambda x: 0 if x < q_low_lgbm else x)
+    sub_lgbm['TARGET'] = sub_lgbm['TARGET'].apply(lambda x: 1 if x > q_high_lgbm else x)
+
+    train_df['xgb'] = train_df['xgb'].apply(lambda x: 0 if x < q_low_xgb else x)
+    train_df['xgb'] = train_df['xgb'].apply(lambda x: 1 if x > q_high_xgb else x)
+
+    sub_xgb['TARGET'] = sub_xgb['TARGET'].apply(lambda x: 0 if x < q_low_xgb else x)
+    sub_xgb['TARGET'] = sub_xgb['TARGET'].apply(lambda x: 1 if x > q_high_xgb else x)
+
     # find best weights
     auc_bst = 0.0
     for w in np.arange(0,1.001, 0.001):
@@ -60,17 +93,16 @@ def main():
             auc_bst = _auc
             w_bst = (w, 1.0-w)
         if np.mod(w*1000, 10)==0:
-            print("w: {}, auc: {}".format(w, _auc))
+            print("w: {:.3f}, auc: {:.10f}".format(w, _auc))
 
-    print("best w: {}, best auc: {}".format(w_bst, auc_bst))
-    """
+    print("best w: {}, best auc: {:.10f}".format(w_bst, auc_bst))
 
-    # take average of each prediction
+    # take weighted average of each prediction
     sub['lgbm'] = sub_lgbm['TARGET']
     sub['xgb'] = sub_xgb['TARGET']
 
-#    sub['TARGET'] = w_bst[0]*sub_lgbm['TARGET'] + w_bst[1]*sub_xgb['TARGET']
-    sub['TARGET'] = 0.5*sub_lgbm['TARGET'] + 0.5*sub_xgb['TARGET']
+    sub['TARGET'] = w_bst[0]*sub_lgbm['TARGET'] + w_bst[1]*sub_xgb['TARGET']
+#    sub['TARGET'] = 0.5*sub_lgbm['TARGET'] + 0.5*sub_xgb['TARGET']
 
     # save submission file
     sub[['SK_ID_CURR', 'TARGET']].to_csv('submission_blend.csv', index= False)
